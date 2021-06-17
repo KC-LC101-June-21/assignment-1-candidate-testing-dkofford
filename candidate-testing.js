41819ce74ed63d0bf93e308540bda7c6700dad75
Drayton Kofford <84445659+dkofford@users.noreply.github.com>
2021-06-17 08:00:32 +0000

    Pretty much a fully running program i just do not have the functions running yet

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,48 +1,66 @@
 const input = require('readline-sync');
 
-// TODO 2: modify your quiz app to ask 5 questions //fd
-
-// TODO 1.1a: Define candidateName // 
-let name = input.question("enter your name:")
-console.log( "Candidate Name: " + name);
-
-// TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-
 /// define all questions 
+let name 
 let question = "1) Who was the first American woman in space? ";
 let question2 = "2)True or False: 5 Kilometes == 5000 meters?";
-let question3 = "(5 + 3)/2 * 10 = ?";
-let question4 = "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?  ";
-let question5 = "What is the minimum crew size for the ISS? ";
+let question3 = "3)(5 + 3)/2 * 10 = ?";
+let question4 = "4)Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?  ";
+let question5 = "5)What is the minimum crew size for the ISS? ";
 
 ///define all correct ansewers 
-let correctAnswer = "Sally Ride";
-let correctAnswer2 = "true";
-let correctAnswer3 = 40 ;
-let correctAnswer4 = "Trajectory";
-let correctAnswer5 = "3";
+let correctAnswer = 'Sally Ride';
+let correctAnswer2 = 'true';
+let correctAnswer3 = '40' ;
+let correctAnswer4 = 'Trajectory';
+let correctAnswer5 = '3';
 
 //Define all Arrays 
 
 let questionarray = [question,question2,question3,question4,question5]
+let correctanswerarray = [correctAnswer,correctAnswer2,correctAnswer3,correctAnswer4 ,correctAnswer5]
 let question4array = [8, 'Orbit', 'Trajectory', 45]
-let ansewerarray = ["answer1","answer2","answer3","answer4","answer5"]
+let ansewerarray = []
+let x = 0 
+
+
+/// ask for the name 
+    name = input.question("enter your name:")
+    console.log("Candidate Name: " + name);
+
+//// ask all questions in a loop 
+
+ for(let i = 0 ; i < questionarray.length ; i++){
+  let answer = input.question(questionarray[i])
+  ansewerarray.push(answer)
+  console.log("Candidate Ansewer: " + answer)
+  console.log( "Correct Ansewer :" + correctanswerarray[i])
+  console.log(" ")
+if ( ansewerarray[i] == correctanswerarray[i]){
+    x = x + 1 
+  }
+}
+
+///// Grade quiz and give feedback 
+
+  console.log(">>> Overall Grade: " + ((x/5)*100) + "% (" + x + " of 5 responses correct) <<<" )
+
+if ( x >= 4 ){ 
+  console.log(">>> Status: PASSED <<<")
+} else{
+  console.log(">>> Status: FAILED <<<")
+}
 
 
 
+console.log(correctanswerarray)
+console.log(ansewerarray)
+console.log(x)
 
-//let candidateAnswer = input.question(question);
-////console.log("Your Answer :" + candidateAnswer);
-///console.log( "Correct Ansewer :" + correctAnswer)
-
-
-let questions;
-let correctAnswers;
-let candidateAnser; 
 
 function askForName() {
-  // TODO 1.1b: Ask for candidate's name //
-
+ 
+ 
 }
 
 function askQuestion() {
@@ -72,6 +90,7 @@ function runProgram() {
 
 // Don't write any code below this line //
 // And don't change these or your program will not run as expected //
+
 module.exports = {
   candidateName: candidateName,
   question: question,
